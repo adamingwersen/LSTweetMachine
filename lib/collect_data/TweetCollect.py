8b8bfd15279bcd51bd15283c550f5365394e4459
adamingwersen <adamlinnemann@gmail.com>
2018-11-20 17:05:19 +0900

    Setup more robust framework: Store Hashtags, Mentions

--- lib/collect_data/TweetCollect.py
+++ lib/collect_data/TweetCollect.py
@@ -6,29 +6,30 @@ from datetime import datetime
 from tweetscrape.profile_tweets import TweetScrapperProfile
 
 
-class TweetCollectUser(sqlite_file, sqlite_table, user_file, e_dir = '../../data/'):
-    def __init__(self):
+class CollectUser:
+    def __init__(self, sqlite_file, sqlite_table, user_file, npage = 2, e_dir = '../../data/'):
         self.sqlite_file = sqlite_file
         self.sqlite_table = sqlite_table
         self.user_file = user_file
+        self.npage = npage
         self.temp_msg = {}
         self.e_dir = e_dir
         self.e_mentions = []
         self.e_hashtags = []
 
-    def cast_unix(strdate):
+    def cast_unix(self, strdate):
         return(str(datetime.fromtimestamp(int(strdate[0:10])).strftime('%Y-%m-%d %H:%M:%S')))
 
-    def fetch_profiles(filename):
+    def fetch_profiles(self, filename):
         f           = open(filename, 'r')
         profiles    = f.read().splitlines()
         f.close()
         return(list(set(profiles)))
 
-    def build_dict(TweetInfo):
+    def build_dict(self, TweetInfo):
         result = {}
         result['Id']        = TweetInfo.get_tweet_id()
-        result['Time']      = '"{}"'.format(cast_unix(TweetInfo.get_tweet_time_ms()))
+        result['Time']      = '"{}"'.format(self.cast_unix(TweetInfo.get_tweet_time_ms()))
         result['Author']    = '"{}"'.format(TweetInfo.get_tweet_author())
         result['Text']      = '"{}"'.format(TweetInfo.get_tweet_text())
         result['Hashtags']  = '"{}"'.format(TweetInfo.get_tweet_hashtags())
@@ -42,7 +43,7 @@ class TweetCollectUser(sqlite_file, sqlite_table, user_file, e_dir = '../../data
         self.e_hashtags.extend(result['Hashtags'])
         return(result)
 
-    def build_query(table, ddict):
+    def build_query(self, table, ddict):
         cols = ''
         vals = ''
         for key, val in ddict.items():
@@ -55,21 +56,29 @@ class TweetCollectUser(sqlite_file, sqlite_table, user_file, e_dir = '../../data
         query = 'INSERT INTO {} ({}) VALUES ({})'.format(table, cols, vals)
         return(query)
 
-    def picklelist(item, path):
+    def picklelist(self, item, path):
         with open(path, 'wb') as fp:
             pickle.dump(item, fp)
 
-    def collect(recursive, npage):
-        profiles = fetch_profiles(self.user_file)
+    def fetch_tweets(self, profile, npage):
+        TS = TweetScrapperProfile(profile, npage)
+        tweets = TS.get_profile_tweets()
+        return(tweets)
+
+    def collect(self):
+        profiles = self.fetch_profiles(self.user_file)
         cnxn = sqlite3.connect(self.sqlite_file)
         c = cnxn.cursor()
         for profile in profiles:
-            tweets = fetch_tweets(profile, npage)
+            tweets = self.fetch_tweets(profile, self.npage)
             for tweet in tweets:
-                q = build_query(table_name, build_dict(tweet))
-                c.execute(q)
-                print("Tweet inserted: {}".format{self.temp_msg})
+                q = self.build_query(self.sqlite_table, self.build_dict(tweet))
+                try:
+                    c.execute(q)
+                except sqlite3.OperationalError as e:
+                    print(e, q)
+                print("Tweet inserted: {}".format(self.temp_msg))
             cnxn.commit()
         cnxn.close()
-        picklelist(self.e_hashtags, self.e_dir + 'hashtags.pickle')
-        picklelist(self.e_mentions, self.e_dir + 'mentions.pickle')
+        self.picklelist(self.e_hashtags, self.e_dir + 'hashtags.pickle')
+        self.picklelist(self.e_mentions, self.e_dir + 'mentions.pickle')
