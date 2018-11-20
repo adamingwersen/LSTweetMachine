613b86b9e074fc5d3c07d223193957bf86fbd5ba
adamingwersen <adamlinnemann@gmail.com>
2018-11-20 17:14:33 +0900

    Get em all

--- lib/collect_data/get_tweets.py
+++ lib/collect_data/get_tweets.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     sqlite_file     = '../../data/database/deeplearning.sqlite'
     sqlite_table    = 'tweets'
     user_file       = '../../data/profiles.txt'
-    clct = tc.CollectUser(sqlite_file, sqlite_table, user_file, npage = 2)
+    clct = tc.CollectUser(sqlite_file, sqlite_table, user_file, npage = 25)
     clct.collect()
